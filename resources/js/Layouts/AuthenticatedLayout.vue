df9f8f5fddb8d45e9d2789a9beb8758ce674f26d
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-02-29 21:09:32 +0900

    :construction: Buttonの調整から

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -137,7 +137,7 @@ const showingNavigationDropdown = ref(false);
             </nav>
 
             <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
+            <header class="bg-slate-950 shadow" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
--- resources/js/Pages/News/List.vue
+++ resources/js/Pages/News/List.vue
@@ -12,19 +12,27 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">お知らせ機能</h2>
-            <h2 class="font-semibold text-l text-gray-500 leading-tight">お知らせ一覧</h2>
+            <h2 class="font-semibold text-xl text-slate-50 leading-tight">お知らせ機能</h2>
+            <h2 class="font-semibold text-l text-slate-50 leading-tight">お知らせ一覧</h2>
         </template>
 
-        <div v-for="n in news" :key="n.id">
-            <div class="py-2">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div>{{ n.public_date }}</div>
-                        <Link :href="route('news.detail')" class="">{{ n.title }}</Link>
-                        <div v-if="n.status === 'public'">公開</div>
-                        <div v-if="n.status === 'private'">非公開</div>
-                        <div>編集</div>
+        <div v-if="news" class="max-w-7xl my-10 mx-auto py-10 rounded-2xl bg-slate-300">
+            <div v-for="n in news" :key="n.id" class="">
+                <div class="py-2">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="flex bg-white py-6 overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="mx-8">{{ n.public_date }}</div>
+                            <Link :href="route('news.detail')" class="flex-1">{{ n.title }}</Link>
+                            <div v-if="n.status === 'public'" class="flex">
+                                <div class="bg-slate-400 py-1 px-3 border rounded-xl me-6">公開</div>
+                                <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">非公開</div>
+                            </div>
+                            <div v-if="n.status === 'private'" class="flex">
+                                <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">公開</div>
+                                <div class="bg-slate-400 py-1 px-3 border rounded-xl me-6">非公開</div>
+                            </div>
+                            <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">編集</div>
+                        </div>
                     </div>
                 </div>
             </div>
