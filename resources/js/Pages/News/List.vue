389c76ee49fe925cf30d38cfdbe288e57aaff740
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-03-04 18:57:22 +0900

    :lipstick: お知らせ一覧のCordingを反映しました。

--- resources/js/Pages/News/List.vue
+++ resources/js/Pages/News/List.vue
@@ -12,26 +12,29 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-slate-50 leading-tight">お知らせ機能</h2>
-            <h2 class="font-semibold text-l text-slate-50 leading-tight">お知らせ一覧</h2>
+            <div class="flex justify-between">
+                <div class="text-slate-50 font-semibold text-xl">お知らせ機能</div>
+                <div class="text-slate-50 font-semibold text-2xl">お知らせ一覧</div>
+                <a href="#" class="bg-slate-50 py-1 px-3 font-semibold rounded-xl hover:bg-slate-500 hover:text-white transition">新規作成</a>
+            </div>
         </template>
 
         <div v-if="news" class="max-w-7xl my-10 mx-auto py-10 rounded-2xl bg-slate-300">
             <div v-for="n in news" :key="n.id" class="">
                 <div class="py-2">
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="flex bg-white py-6 overflow-hidden shadow-sm sm:rounded-lg">
-                            <div class="mx-8">{{ n.public_date }}</div>
-                            <Link :href="route('news.detail')" class="flex-1">{{ n.title }}</Link>
+                    <div class="max-w-7xl px-8 mx-auto">
+                        <div class="flex bg-white py-6 rounded-lg overflow-hidden shadow-lg hover:bg-slate-100 transition">
+                            <div class="mx-8 py-1">{{ n.public_date }}</div>
+                            <Link :href="route('news.detail')" class="flex-1 py-1 underline">{{ n.title }}</Link>
                             <div v-if="n.status === 'public'" class="flex">
-                                <div class="bg-slate-400 py-1 px-3 border rounded-xl me-6">公開</div>
-                                <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">非公開</div>
+                                <a href="#" class="bg-slate-500 text-white py-1 px-3 me-6 border rounded-2xl hover:bg-slate-900 transition">公開</a>
+                                <a href="#" class="bg-slate-50 py-1 px-3 me-6 border rounded-2xl hover:bg-slate-900 hover:text-white transition">非公開</a>
                             </div>
                             <div v-if="n.status === 'private'" class="flex">
-                                <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">公開</div>
-                                <div class="bg-slate-400 py-1 px-3 border rounded-xl me-6">非公開</div>
+                                <a href="#" class="bg-slate-50 py-1 px-3 me-6 border rounded-2xl hover:bg-slate-900 hover:text-white transition">公開</a>
+                                <a href="#" class="bg-slate-500 text-white py-1 px-3 me-6 border rounded-2xl hover:bg-slate-900 transition">非公開</a>
                             </div>
-                            <div class="bg-slate-50 py-1 px-3 border rounded-xl me-6">編集</div>
+                            <a href="#" class="bg-blue-100 text-blue-900 py-1 px-3 me-6 border hover:bg-blue-900 hover:text-white transition">編集</a>
                         </div>
                     </div>
                 </div>
