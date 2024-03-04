5c834755329d5e1bc5387cb4b4aeda13fbe1bf82
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-03-04 19:14:40 +0900

    :lipstick: <管理画面>お知らせ一覧のCordingに削除を追加しました。

--- resources/js/Pages/News/List.vue
+++ resources/js/Pages/News/List.vue
@@ -35,6 +35,7 @@ defineProps({
                                 <a href="#" class="bg-slate-500 text-white py-1 px-3 me-6 border rounded-2xl hover:bg-slate-900 transition">非公開</a>
                             </div>
                             <a href="#" class="bg-blue-100 text-blue-900 py-1 px-3 me-6 border hover:bg-blue-900 hover:text-white transition">編集</a>
+                            <a href="#" class="bg-red-100 text-red-900 py-1 px-3 me-6 border hover:bg-red-900 hover:text-white transition">削除</a>
                         </div>
                     </div>
                 </div>
