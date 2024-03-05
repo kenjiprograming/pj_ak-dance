489528f69692d6581f9aa6f74140f353edaec3b5
kenjiPro <iamkenji.programing@gmail.com>
2024-03-05 19:42:49 +0900

    :sparkles: <管理画面>機能一覧を実装しました。

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FeatureController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -29,6 +30,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/feature', [FeatureController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('feature');
 
 Route::get('/news', [NewsController::class, 'index'])
     ->middleware(['auth', 'verified'])
