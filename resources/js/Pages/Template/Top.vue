d710b4eb85deae77a1caf226f931b485c32ceef5
kenjiPro <iamkenji.programing@gmail.com>
2024-03-09 19:07:11 +0900

    :lipstick: <表側>sp header

---
+++ resources/js/Pages/Template/Top.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+let news = ref('')
+
+
+const toggleHamburger = () => {
+    const bars =document.getElementById('bars');
+    const xmark =document.getElementById('xmark');
+    const menu = document.getElementById('menu');
+
+    bars.classList.toggle('hidden')
+    xmark.classList.toggle('hidden')
+    menu.classList.toggle('hidden')
+}
+
+</script>
+
+<template>
+    <Head title="AK-DANCE" />
+
+    <header >
+        <div class="
+            bg-black h-auto
+            flex justify-between
+            ">
+            <a href="./index.html" class="sm:hidden" >
+                <img src="/images/icon.svg" alt="">
+            </a>
+            <a href="./index.html" class="lg:hidden" >
+                <img src="/images/icon-sp.svg" alt="">
+            </a>
+
+            <a href="./index.html" class="sm:hidden">
+                <img src="/images/logo.svg" alt="">
+            </a>
+            <a href="./index.html" class="lg:hidden">
+                <img src="/images/logo-sp.svg" alt="">
+            </a>
+
+            <div class="sm:hidden">
+                <a href="./index.html#news-wrapper" class="gnavi-item link">お知らせ</a>
+                <a href="./index.html#career-wrapper" class="gnavi-item link">講師</a>
+                <a href="./index.html#concept-wrapper" class="gnavi-item link">コンセプト</a>
+                <a href="./index.html#schedule-wrapper" class="gnavi-item link">レッスン</a>
+                <a href="./index.html#access-wrapper" class="gnavi-item link">アクセス</a>
+                <a href="./index.html#contact-wrapper" class="gnavi-item link">お問い合わせ</a>
+            </div>
+            <div id="hamburger" @click="toggleHamburger()" class="lg:hidden me-2">
+                <img id="bars" class="h-full" src="/images/hamburger-close.svg" alt="">
+                <img id="xmark" class="hidden h-full" src="/images/hamburger-open.svg" alt="">
+            </div>
+        </div>
+        <div id="menu" class="
+            lg:hidden bg-black
+            hidden
+            flex flex-col
+            py-4
+            h-auto
+            ">
+            <div class="flex justify-center my-4">
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#news-wrapper">お知らせ</a>
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#career-wrapper">講師</a>
+            </div>
+            <div class="flex justify-center my-4">
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#concept-wrapper">コンセプト</a>
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#schedule-wrapper">レッスン</a>
+            </div>
+            <div class="flex justify-center my-4">
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#access-wrapper">アクセス</a>
+                <a class="w-40 py-4 mx-8 text-white text-center border" href="./index.html#contact-wrapper">お問い合わせ</a>
+            </div>
+        </div>
+    </header>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/top', function () {
+    return Inertia::render('Template/Top');
+})->name('top');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
