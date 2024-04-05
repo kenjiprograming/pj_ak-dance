cc9f94c066531aef597ce1272153aad9cc46ee23
iamkenji.programing@gmail.com <i.am.kenji.gido@gmail.com>
2024-04-05 17:37:09 +0900

    :wrench: Description, keyword, favicon, app-icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title inertia>{{ config('app.name', 'AK-DANCE') }}</title>
+
         <meta charset="utf-8">
+        <meta name="keywords" content="AK-DANCE,AIKA,ダンス,ダンススクール,ダンスレッスン"/>
+        <meta name="description" content="AK-DANCEは、静岡県浜松市を拠点にダンサーAIKAがレッスンを行うダンススクールです。AK-DANCEの新着情報やコンセプト、レッスン日程、レッスン会場、また、AIKAの経歴等をご覧いただけます。"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" type="image/vnd.microsoft.icon"/>
+        <link rel="icon" href="/images/favicon.ico" type="image/vnd.microsoft.icon"/>
+        <link rel="apple-touch-icon" href="/images/app-icon.png"/>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
